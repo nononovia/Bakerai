e635860c697d7c8a3eee8d427f292001b36cb637
EliasPinno <pucas946@gmail.com>
2021-02-28 21:19:53 -0800

    Cleaned up user input for later use

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 #Write a message introducing the chatbot, and print the message to the console
 print("Hello! This is the chatbot. I am here to boost your mental wellbeing! Let's chat:", flush = True)
 #Write a loop to repeatedly prompt the user for input, and store that input in a variable. (variable as a function input later)
 #terminate the loop after the user inputs a reserved value of your choosing
-ill = "stressed"
+END_CONVO = "0"
 i = True
 while i == True:
-    reading = input("One word to describe how you feel today:")
-    if reading == ill:
+    reading = input()
+    if reading == END_CONVO:
         i = False
-    else:
+    else: # Send response to user
         print("E\n")
     #save that reserved value to a constant outside of the "main" method
-global value
-value = ill
-
