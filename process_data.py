d23d9883cfa1a5ac6e0eacca8eff775994b6ac8f
EliasPinno <pucas946@gmail.com>
2021-03-08 01:38:04 -0800

    Turned data into bags of words (one hot encoding)

--- process_data.py
+++ process_data.py
@@ -12,7 +12,8 @@ with open("intents.json") as jsonFile:
 
 allWords = []
 convoLabels = []
-docs = []
+docs_x = []
+docs_pattern = []
 
 for intent in data["intents"]: # For each "type" of conversation
     if intent["tag"] not in convoLabels:
@@ -20,8 +21,42 @@ for intent in data["intents"]: # For each "type" of conversation
     for pattern in intent["patterns"]: # for each input pattern
         # Stemming: take each word, get "root" word to make learning more accurate
 
-        # To start, get the words with nltk magic
+        # To start, get the words and remove extra with nltk magic
         tempWrds = nltk.word_tokenize(pattern)
         allWords.extend(tempWrds) # Append all words at once
-        docs.append(pattern)
         
+        # Store practice inputs and their classifications
+        docs.append(pattern)
+        docs_pattern.append(intent["tag"])
+# Turn all words to lowercase
+allWords = [stemmer.stem(w.lower()) for w in allWords]
+
+# Remove all duplicates, and sort words (for easier use)
+allWords = sorted(list(set(words)))
+
+convoLabels = convoLabels.sort()
+
+training = []
+output = []
+
+out_empty = [0]*len(convoLabels)
+
+# Idea: take each sentence, turn it into a list of word occurances, with a list of all possible words we are looking at
+for i, doc in enumerate(docs_x):
+    bag = []
+    # Simplify words to the root word
+    wrds = [stemmer.stem(w) for w in doc] 
+
+    for word in allWords: 
+        if word in wrds: # This word is in our sentence
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empty[:]
+    output_row = [labels.index(docs_patter[i])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+# Turn data to numpy arrays
+training = numpy.array(training)
+output = np.array(output)
