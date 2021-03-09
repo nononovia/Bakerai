720100a9b7e78c8e148c813f16d7a522485c3610
Rick Feng <rickfeng99@gmail.com>
2021-03-09 00:21:17 -0800

    last edit

--- NN.py
+++ NN.py
@@ -38,17 +38,3 @@ except:
     # we pass in 8 batch at a time
 
     model.save("model.tflearn")
-
-
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-            
-    return numpy.array(bag)
