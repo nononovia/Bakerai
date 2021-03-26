2f70ea72d37ee4f351543cbf5f2f312b22db1247
EliasPinno <pucas946@gmail.com>
2021-03-26 02:41:29 -0700

    Made GUI get read only replies from bot on local machine

--- clientGUI.py
+++ clientGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import main as m
 
 FONT = ("Verdana",12)
 
@@ -19,27 +20,40 @@ class bakerClient(tk.Tk):
         
         mainFrame = tk.Frame(parent)
         mainFrame.pack(side="top",fill="both",expand=True)
-        for i in range(7):
+        for i in range(9):
             mainFrame.grid_rowconfigure(index=i, weight=1)
             mainFrame.grid_columnconfigure(index=i, weight=1)
+        self.loaded_clf = m.load_sentiment_analysis()[0]
 
 
         title = tk.Label(mainFrame, text="Welcome to Sakura's very own BakerAI!", font=FONT)
-        title.grid(row=0,column=0,columnspan=7)
+        title.grid(row=1,column=0,columnspan=7)
         # What we will change to show output
-        self.outputLabel = tk.Label(mainFrame, text="Where Bot will Reply", font=FONT)
-        self.outputLabel.grid(row=3, column=0, columnspan=2, sticky="NSEW")
+        self.outputBox = tk.Text(mainFrame, font=FONT)
+        self.outputBox.grid(row=3, column=1, columnspan=7, rowspan=1, sticky="")
+        self.outputBox.configure(state="disabled")
+        scrollbar = tk.Scrollbar(self)
+        scrollbar.config(command = self.outputBox.yview)
+        self.outputBox.config(yscrollcommand=scrollbar.set)
         # Input related items
         self.userInput = tk.Entry(mainFrame)
         self.userInput.grid(row=5, column=1, columnspan=4, sticky="EW")
         sendButton = tk.Button(mainFrame, text="Send message", command=lambda: self.getResponse())
-        sendButton.grid(row=5, column=6, columnspan=2, sticky="EW")
+        sendButton.grid(row=5, column=7, columnspan=2, sticky="EW")
 
     def getResponse(self):
         # Get user message
         userMessage = self.userInput.get()
         # Clear the user input
         self.userInput.delete(0, "end")
+        # Get our reply
+        reply = "BakerAI: " + m.getFinalOutput(self.loaded_clf,userMessage) + "\n"
+        # Send reply to client
+        storedUserMessage = "You: " + userMessage + "\n"
+        self.outputBox.configure(state="normal")
+        self.outputBox.insert(tk.END, storedUserMessage) 
+        self.outputBox.insert(tk.END, reply) 
+        self.outputBox.configure(state="disabled")
 
 
 if __name__ == '__main__':
