780ce483a3fcc670955d1abc5581b7beec9d2c7d
EliasPinno <pucas946@gmail.com>
2021-03-29 17:42:30 -0700

    Laid out testing framework

---
+++ unit-testing.py
@@ -0,0 +1,13 @@
+import unittest
+class testBotMethods(unittest.TestCase):
+    def __init__(self):
+        # Take in basic paramaters
+    def testIntent():
+        # ensure an extremely negative response is negative
+    def testResponses():
+        # ensure we can get desired responses for very typical questions
+    def testDefault(): 
+        # ensure we can get a default response when not discussing any subjects
+
+if __name__ == '__main__':
+    unittest.main()
