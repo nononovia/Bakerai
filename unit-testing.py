28fe0d1c383c3f00ed819afa0db978e043b959a2
EliasPinno <pucas946@gmail.com>
2021-03-29 17:49:51 -0700

    More updates to unit testing frameworks

--- unit-testing.py
+++ unit-testing.py
@@ -1,11 +1,19 @@
 import unittest
+import main as m
+# Define some constants which we may use for our testing
+NEGATIVE_MESSAGE = ""
+
 class testBotMethods(unittest.TestCase):
     def __init__(self):
+        self.loaded_clf = load_sentiment_analysis()[0]
         # Take in basic paramaters
+        
     def testIntent():
         # ensure an extremely negative response is negative
+
     def testResponses():
         # ensure we can get desired responses for very typical questions
+        
     def testDefault(): 
         # ensure we can get a default response when not discussing any subjects
 
