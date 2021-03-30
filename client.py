87decf6a5e5b03f37be85ab317b0fdc0f1233d06
EliasPinno <pucas946@gmail.com>
2021-03-29 21:09:25 -0700

    Added loads of documentation to client and server.py

--- client.py
+++ client.py
@@ -3,18 +3,23 @@ class ourClient():
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+    # Connects to a server
     def connect(self, connectInfo):
         self.sock.connect(connectInfo)
     
+    # Gets a response in readable form from a server
     def getResponse(self):
         return self.sock.recv(4096).decode("utf-8")
 
+    # Sends string response correctly encoded to the server
     def sendResponse(self, msg):
         self.sock.send(bytes(msg.encode()))
     
+    # Closes the connection to the server
     def close(self):
         self.sock.close()
     
+    # Opens a client connection with a server until closing
     def runClient(self):
         ip = input ("enter target IP Address: ")
         port = int(input("enter target port: "))
@@ -26,7 +31,7 @@ class ourClient():
             response = client.getResponse()
             print("Server response: " + response)
         client.close()
-
+# If this file is run as main
 if __name__ == "__main__":
     client = ourClient()
     client.runClient()
